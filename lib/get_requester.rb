a77df506d090315358fef8e6c062a0f5a5ede615
AHMED AFANDY <eng.computer1@yahoo.com>
2020-04-27 18:32:57 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -13,8 +13,7 @@ class GetRequester
   def get_response_body
     uri = URI.parse(@url)
     #data=  uri.open.string
-    response = Net::HTTP.get_response(uri) #second way
-    binding.pry
+    response = Net::HTTP.get_response(uri)
     response.body
   end
 
